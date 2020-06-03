a30856830e4c3a6db258727032ad56ad2c26052b
Alan Vieyra <alan.vieyra376@gmail.com>
2020-06-03 06:43:17 -0500

    Add hiredis with OMP

---
+++ src/config.h
@@ -0,0 +1,133 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+#include <omp.h>
+#include <vector>
+
+#include <hiredis.h>
+
+#include "lib/log.h"
+#include "lib/FileWatcher.h"
+
+#define VERSION "1.8.8"
+#define MAXCHAR 300
+#define WORKDIR "/data"
+#define DATA_BLOCK 10000000  // Each DATA_BLOCK reg. reconnect to Redis server.
+#define U_SLEEP 5            // Sleep 5us.
+
+std::vector<redisContext*> c;
+const char* filename;
+
+void read_file();
+
+void sigint_handler(int sig_no) {
+  /* Disconnects and frees the context */
+  for (int i = 0; i < c.size(); i++) {
+    redisFree(c[i]);
+  }
+
+  signal(SIGINT, SIG_DFL);
+  kill(getpid(), SIGINT);
+}
+
+void signal_init() {
+  signal(SIGINT, sigint_handler);
+  signal(SIGTERM, sigint_handler);
+  signal(SIGPIPE, SIG_IGN);
+}
+
+void log_init(){
+  char logFilename[MAXCHAR];
+  strcpy(logFilename, WORKDIR);
+  strcat(logFilename, "/log/redis_data_load.log");
+
+  LOG_CONFIG log_conf = {9, LOG_DEST_FILES, logFilename, "redis_data_load", 0, 1};
+  log_set_config(&log_conf);
+}
+
+void file_check(const char* filename) {
+  FILE *file = fopen(filename, "r");
+
+  if (file != NULL) {
+    fclose(file);
+
+    read_file();
+
+    log_(L_INFO | L_CONS, "Escuchando cambios en el archivo: %s\n", filename);
+  } else {
+    log_(L_INFO | L_CONS, "Esperando a la creacion del archivo: %s\n",
+        filename);
+  }
+
+  log_(L_INFO | L_CONS, "...\n");
+}
+
+void file_watcher_init() {
+  file_check(filename);
+  // Create a FileWatcher instance that will check the current folder for
+  // changes every 5 seconds
+  FileWatcher fw{WORKDIR, std::chrono::milliseconds(1000)};
+
+  // Start monitoring a folder for changes and (in case of changes)
+  // run a user provided lambda function
+  fw.start([](std::string path_to_watch, FileStatus status) -> void {
+    // Process only regular files, all other file types are ignored
+    if (!std::filesystem::is_regular_file(
+            std::filesystem::path(path_to_watch)) &&
+        status != FileStatus::erased) {
+      return;
+    }
+
+    switch (status) {
+      case FileStatus::created:
+      case FileStatus::modified:
+        if (strcmp(path_to_watch.c_str(), filename) == 0) {
+          log_(L_INFO | L_CONS, "Archivo creado/modificado: %s\n",
+               path_to_watch.c_str());
+          file_check(filename);
+        }
+        break;
+      default:
+        break;
+    }
+  });
+}
+
+redisContext *redis_init(const char* hostname, int port, const char* password, int database) {
+  struct timeval timeout = { 1, 500000 }; // 1.5 seconds
+  redisContext *c_new = redisConnectWithTimeout(hostname, port, timeout);
+  redisReply *reply;
+  
+  if (c_new == NULL || c_new->err) {
+    if (c_new) {
+      log_(L_ERROR, "Connection error: %s\n", c_new->errstr);
+      redisFree(c_new);
+    } else {
+      log_(L_ERROR, "Connection error: can't allocate redis context\n");
+    }
+
+    exit(1);
+  }
+
+  c.push_back(c_new);
+
+  reply = (redisReply *) redisCommand(c_new,"AUTH %s", password);
+  if (strcmp(reply->str, "OK") == 0) {
+    log_(L_INFO | L_CONS, "[%d] Conectado.\n", omp_get_thread_num());
+  } else {
+    log_(L_WARN | L_CONS, "[%d] No conectado.\n", omp_get_thread_num());
+  }
+  freeReplyObject(reply);
+
+  reply = (redisReply *) redisCommand(c_new,"SELECT %d", database);
+  // log_(L_INFO | L_CONS, "SELECT: %s\n", reply->str);
+  freeReplyObject(reply);
+  
+  return c_new;
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <adapters/libev.h>
+
+#include "config.h"
+
+const char *redis_host;
+int redis_port;
+const char *redis_pass;
+int redis_database;
+
+int get_lines(const char *filename){
+  int lines = 0;
+  long phone;
+  char value[MAXCHAR];
+
+  FILE *file = fopen(filename, "r");
+  if (file == NULL) return -1;
+
+  while (fscanf(file, "%ld|%[^\n]^\n", &phone, value) != EOF) lines++;
+
+  fclose(file);
+
+  return lines;
+}
+
+void read_file() {
+  int lines;
+  int phone_count = 0;
+
+  log_(L_INFO | L_CONS, "Cargando registros de %s...\n", filename);
+
+  lines = get_lines(filename);
+
+  //log_(L_DEBUG, "Lineas: %d\n", lines);
+
+  // Thread management.
+#pragma omp parallel shared(phone_count)
+  {
+    int nt = omp_get_num_threads();
+    int iam = omp_get_thread_num();
+    if ( iam == 0 ) log_(L_INFO | L_CONS, "Hilos usados: %d\n", nt);
+    int th_block = lines / nt;
+    int line_ini = th_block * iam;
+    int line_end = iam != nt - 1 ? th_block * (iam + 1) : lines;
+
+    long phone_ini, phone_end;
+    char key[11];
+    char value[MAXCHAR];
+    char line[MAXCHAR];
+
+    redisContext *ac = redis_init(redis_host, redis_port, redis_pass, redis_database);
+    redisReply *reply;
+
+    FILE *file = fopen(filename, "r");
+
+    for (int curr_line = 0; curr_line < line_ini ; curr_line++) fgets(line, MAXCHAR, file);
+
+    //log_(L_DEBUG | L_CONS, "%d) Lines: %d - %d\n", iam, line_ini, line_end);
+
+    int i = 0;
+    // Expansion de registros en el intervalo.
+    for (i = line_ini; i < line_end; i++) {
+      fscanf(file, "%ld|%ld|%[^\n]s", &phone_ini, &phone_end, value);
+
+      if (phone_end - phone_ini < 10000) {
+#pragma omp critical
+        phone_count += phone_end - phone_ini + 1;
+
+        //log_(L_DEBUG | L_CONS, "%d) phones: %ld - %ld\n", iam, phone_ini, phone_end);
+
+        for (long phone = phone_ini; phone <= phone_end; phone++) {
+          sprintf(key, "%ld", phone);
+
+          /* Cargar a Redis */
+          reply = (redisReply *) redisCommand(ac, "SET %s %s", key, value);
+          freeReplyObject(reply);
+        }
+      }
+    }
+
+    fclose(file);
+
+    /* Disconnect redis sock */
+    redisFree(ac);
+  }
+
+  log_(L_INFO | L_CONS, "Carga completa. Total de registros: %ld\n",
+       phone_count);
+}
+
+int main(int argc, char **argv)
+{
+  fprintf(stderr, "#############################\n");
+  fprintf(stderr, "# Data load - Series v%s #\n", VERSION);
+  fprintf(stderr, "#############################\n\n");
+
+  if (argc < 5) {
+    log_(L_WARN,
+         "Use: ./main.o <REDIS_HOST> <REDIS_PORT> <REDIS_PASS> <REDIS_DATABASE> <FILE_NAME>\n");
+    exit(1);
+  }
+
+  redis_host = (argc > 1) ? argv[1] : "127.0.0.1";
+  redis_port = (argc > 2) ? atoi(argv[2]) : 6379;
+  redis_pass = (argc > 3) ? argv[3] : "";
+  redis_database = (argc > 4) ? atoi(argv[4]) : 0;
+  filename = (argc > 5) ? argv[5] : "";
+
+  log_init();
+  signal_init();
+  log_(L_INFO | L_CONS, "Inicializado correctamente.\n");
+
+  file_watcher_init();
+  //ev_loop(EV_DEFAULT_ 0);
+
+  return 0;
+}
